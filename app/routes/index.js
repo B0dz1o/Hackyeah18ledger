eaa20ab4afe9b37fb3cdae6ceee8691e76c05286
Piotr Bogdan <bogdzio@o2.pl>
2018-11-25 11:35:23 +0100

    Finally working version!

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var LocalStorage = require('node-localstorage').LocalStorage,
 localStorage = new LocalStorage('./balances');
-var query = require('../query')
+var query = require('../query');
+var invoke = require('../invoke');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,23 +11,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/addPoints/:points/for/:userName', (req, res, next) => {
-  var userName = req.params.userName;
   var pointsAdded = Number.parseInt(req.params.points, 10);
-  var marshalled = localStorage.getItem(userName);
-  var userData = JSON.parse(marshalled);
-  if (userData === null) {
-    userData = { userName, transactions: [], balance: 0}
-  }
-  userData.transactions.push({
-    pointsAdded,
-    date: new Date()
-  });
-  var prevBalance = userData.balance
-  var currentBalance = prevBalance + pointsAdded;
-  userData.balance = currentBalance;
-  console.log(JSON.stringify(userData));
-  localStorage.setItem(userName, JSON.stringify(userData));
-  res.send({ pointsAdded, currentBalance, userName });
+
+  var userName = req.params.userName;
+  query.getAllCustomers().then( data => {
+    let dataOb = JSON.parse(data)
+    let user = dataOb.filter( (v,i,a) => {
+      return v.Record.userName === userName
+    })[0]
+    var key = user.Key;
+    console.log(typeof key);
+    user.Record.transactions.push( {
+      pointsAdded,
+      date: new Date()
+    });
+    user.Record.balance += pointsAdded;
+    invoke.saveCustomer(key, user).then ( data => {
+      res.send({ user: user.Record });
+    })
+  })
+
+
 });
 
 router.get('/currentBalanceOf/:userName', (req, res, next) => {
