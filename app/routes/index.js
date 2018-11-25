fa746eea0692da37de99101c31e953a7fa6e832b
Piotr Bogdan <bogdzio@o2.pl>
2018-11-25 11:19:50 +0100

    Getting data from blockchain
    Mock no more!

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var LocalStorage = require('node-localstorage').LocalStorage,
 localStorage = new LocalStorage('./balances');
+var query = require('../query')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,22 +31,31 @@ router.get('/addPoints/:points/for/:userName', (req, res, next) => {
 
 router.get('/currentBalanceOf/:userName', (req, res, next) => {
   var userName = req.params.userName;
-  var userData = JSON.parse(localStorage.getItem(userName));
-  if (userData === null) {
-    userData = { userName, transactions: [], balance: 0}
-  }
-  var currentBalance = userData.balance;
-  res.send({ currentBalance, userName });
+  query.getAllCustomers().then( data => {
+    let dataOb = JSON.parse(data)
+    let user = dataOb.filter( (v,i,a) => {
+      return v.Record.userName === userName
+    })[0].Record
+    res.send({ user });
+  })
+  // var userName = req.params.userName;
+  // var userData = JSON.parse(localStorage.getItem(userName));
+  // if (userData === null) {
+  //   userData = { userName, transactions: [], balance: 0}
+  // }
+  // var currentBalance = userData.balance;
+  // res.send({ currentBalance, userName });
 });
 
 router.get('/transactionHistoryOf/:userName', (req, res, next) => {
   var userName = req.params.userName;
-  var userData = JSON.parse(localStorage.getItem(userName));
-  if (userData === null) {
-    userData = { userName, transactions: [], balance: 0}
-  }
-  var history = userData.transactions
-  res.send({ history });
+  query.getAllCustomers().then( data => {
+    let dataOb = JSON.parse(data)
+    let user = dataOb.filter( (v,i,a) => {
+      return v.Record.userName === userName
+    })[0].Record.transactions
+    res.send({ user });
+  })
 });
 
 module.exports = router;
